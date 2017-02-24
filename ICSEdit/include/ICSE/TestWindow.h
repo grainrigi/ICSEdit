022d3402a3839d04064cb803b99f923edb30326e
NKiryu <naonaokiryu2@gmail.com>
2017-02-24 17:40:44 +0900

    Applied GPLv3

--- ICSEdit/include/ICSE/TestWindow.h
+++ ICSEdit/include/ICSE/TestWindow.h
@@ -1,9 +1,28 @@
+/*
+(c) 2016,2017 Grain
+
+This file is part of ICSEdit.
+
+ICSEdit is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ICSEdit is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ICSEdit.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
+#include "sdl/SDLWindow.h"
 
 namespace ICSE {
-namespace ICSE {
 
-	class TestWindow {		
+	class TestWindow : sdl::SDLWindow {		
 	 public:
 		TestWindow(void);
 		
@@ -11,5 +30,4 @@ namespace ICSE {
 		
 	};
 
-}
 }
---
+++ Readme.md
@@ -0,0 +1,11 @@
+#ICSEdit
+
+"ICSEdit" is the score editor for "ICSSdroid".
+
+###LICENSE
+This software is licensed under GNU Public License v3.
+For the complete license documentation, see "COPYING" file.
+
+##Appreciation
+This software uses "stb single-file public domain libraries for C/C++".
+https://github.com/nothings/stb
