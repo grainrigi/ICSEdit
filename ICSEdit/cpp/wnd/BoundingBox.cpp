89311de438907d0ab5910dc5f9dc2530e9d0aa4b
NKiryu <naonaokiryu2@gmail.com>
2017-03-04 22:00:04 +0900

    Add BoundingBox
    BoundingBox is the class for specifying the space for a control

---
+++ ICSEdit/cpp/wnd/BoundingBox.cpp
@@ -0,0 +1,36 @@
+/* 
+(c) 2016,2017 Grain
+
+This file is part of ICSEdit.
+
+ICSEdit is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ICSEdit is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ICSEdit.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "wnd/BoundingBox.h"
+
+using namespace ICSE::wnd;
+
+void ICSE::wnd::BoundingBox::SetWidth(int nw)
+{
+	if (nw < 0)
+		THROW(std::invalid_argument, "the specified width is below 0.");
+	w = nw;
+}
+
+void ICSE::wnd::BoundingBox::SetHeight(int nh)
+{
+	if (nh < 0)
+		THROW(std::invalid_argument, "the specified height is below 0.");
+	h = nh;
+}
---
+++ ICSEdit/include/wnd/BoundingBox.h
@@ -0,0 +1,46 @@
+/* 
+(c) 2016,2017 Grain
+
+This file is part of ICSEdit.
+
+ICSEdit is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ICSEdit is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ICSEdit.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+namespace ICSE {
+namespace wnd {
+
+	class BoundingBox {
+		int x, y;
+		int w, h;
+	 public:
+		BoundingBox(void);
+
+		int GetX(void) const { return x; }
+		int GetY(void) const { return y; }
+		int GetWidth(void) const { return w; }
+		int GetHeight(void) const { return h; }
+
+		void SetX(int nx) { x = nx; }
+		void SetY(int ny) { y = ny; }
+		void SetWidth(int nw);
+		void SetHeight(int nh);
+		
+	 private:
+		
+	};
+
+}
+}
