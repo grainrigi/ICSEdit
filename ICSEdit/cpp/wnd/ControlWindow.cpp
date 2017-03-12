c73efd33bd60389e3e269a3e3bc866cd6e83c561
NKiryu <naonaokiryu2@gmail.com>
2017-03-13 01:30:59 +0900

    Implemented ControlWindow

--- ICSEdit/cpp/wnd/ControlWindow.cpp
+++ ICSEdit/cpp/wnd/ControlWindow.cpp
@@ -18,5 +18,68 @@ along with ICSEdit.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "wnd/ControlWindow.h"
+#include "graphics/drawenv.h"
 
+using namespace ICSE::sdl;
 using namespace ICSE::wnd;
+using namespace ICSE::graphics;
+
+void ICSE::wnd::ControlWindow::Update(void)
+{
+	dUpdate();
+}
+
+void ICSE::wnd::ControlWindow::MakeCurrent(void)
+{
+	SDL_GL_MakeCurrent(this->window(), m_context);
+}
+
+std::shared_ptr<WindowLayer> ICSE::wnd::ControlWindow::createLayer(void)
+{
+	std::shared_ptr<WindowLayer> layer(new WindowLayer(this, &m_ctlshader));
+	m_layers.insert(std::make_pair(layer->id(), layer));
+	return layer;
+}
+
+void ICSE::wnd::ControlWindow::removeLayer(uint32_t id)
+{
+	auto it = m_layers.find(id);
+	if (it != m_layers.end())
+		m_layers.erase(it);
+}
+
+ICSE::wnd::ControlWindow::ControlWindow(const char *name, uint32_t width, uint32_t height, int flags)
+	: SDLWindow(name, width, height, flags | SDL_WINDOW_OPENGL)
+{
+	initContextAndRenderer();
+	dUpdate += [&]() { this->OnUpdate(); };
+}
+
+void ICSE::wnd::ControlWindow::initContextAndRenderer(void)
+{
+	m_context = SDL_GL_CreateContext(this->window());
+	SDL_GL_MakeCurrent(this->window(), m_context);
+
+	m_m2drend.LateInitialize();
+	m_cvsrend.LateInitialize(&m_m2drend);
+	m_ctlshader.LateInitialize();
+}
+
+void ICSE::wnd::ControlWindow::OnUpdate(void)
+{
+	DrawEnv env;
+	env.setWidth(width());
+	env.setHeight(height());
+
+	this->MakeCurrent();
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	for (auto &layer : m_layers)
+	{
+		layer.second->UpdateAll();
+		layer.second->RenderAll(&env);
+	}
+
+	SDL_GL_SwapWindow(this->window());
+}
--- ICSEdit/include/wnd/ControlWindow.h
+++ ICSEdit/include/wnd/ControlWindow.h
@@ -18,17 +18,44 @@ along with ICSEdit.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
+#include "sdl/SDLWindow.h"
+#include "graphics/Mesh2DRenderer.h"
+#include "graphics/MemCanvasRenderer.h"
+#include "wnd/WindowLayer.h"
+#include "util/basic/Delegate.h"
 
 namespace ICSE {
 namespace wnd {
+	class WindowLayer;
 
-	class ControlWindow {
+	class ControlWindow : public sdl::SDLWindow {
+		//Renderers
+		graphics::Mesh2DRenderer m_m2drend;
+		graphics::MemCanvasRenderer m_cvsrend;
+		ControlRendererShader m_ctlshader;
+
+		SDL_GLContext m_context;
+		std::unordered_map<uint32_t, std::shared_ptr<WindowLayer>> m_layers;
+
+	protected:
 		
+		std::shared_ptr<WindowLayer> createLayer(void);
+		void removeLayer(uint32_t id);
+
+		Delegate<> dUpdate;
+
 	public:
-		ControlWindow(void);
+		ControlWindow(const char *name = "", uint32_t width = 640, uint32_t height = 480, int flags = SDL_WINDOW_SHOWN);
 		
+		graphics::Mesh2DRenderer &GetM2DRenderer(void) { return m_m2drend; }
+		graphics::MemCanvasRenderer &GetCVSRenderer(void) { return m_cvsrend; }
+
+		virtual void Update(void) override;
+
+		void MakeCurrent(void);
 	private:
-		
+		void initContextAndRenderer(void);
+		void OnUpdate(void);
 	};
 
 }
