527f5367366376d2155e7904b4973ee37263ce31
NKiryu <naonaokiryu2@gmail.com>
2017-03-13 01:21:24 +0900

    Fixed incorrect template usage

--- ICSEdit/include/wnd/WindowLayerImpl.h
+++ ICSEdit/include/wnd/WindowLayerImpl.h
@@ -2,7 +2,7 @@
 template<typename Control, typename ...Args>
 inline std::shared_ptr<Control> ICSE::wnd::WindowLayer::CreateControl(Args ...args)
 {
-	static_assert(!std::is_base_of(Control, ICSE::wnd::AWCanvasControl), "CanvasControl must be created by CreateAscentCanvasControl or CreateDescentCanvasControl.");
+	static_assert(!std::is_base_of<ICSE::wnd::AWCanvasControl, Control>::value, "CanvasControl must be created by CreateAscentCanvasControl or CreateDescentCanvasControl.");
 
 	return std::shared_ptr<Control>();
 }
@@ -10,7 +10,7 @@ inline std::shared_ptr<Control> ICSE::wnd::WindowLayer::CreateControl(Args ...ar
 template<typename Control, typename ...Args>
 inline std::shared_ptr<Control> WindowLayer::CreateAscentCanvasControl(Args ...args)
 {
-	static_assert(std::is_base_of(Control, ICSE::wnd::AWCanvasControl), "The Control must inherit AWCanvasControl.");
+	static_assert(std::is_base_of<ICSE::wnd::AWCanvasControl, Control>::value, "The Control must inherit AWCanvasControl.");
 
 	std::shared_ptr<Control> ctl{ new Control(args...) };
 	m_controls.insert(ctl->id(), ctl);
@@ -21,7 +21,7 @@ inline std::shared_ptr<Control> WindowLayer::CreateAscentCanvasControl(Args ...a
 template<typename Control, typename ...Args>
 inline std::shared_ptr<Control> ICSE::wnd::WindowLayer::CreateDescentCanvasControl(Args ...args)
 {
-	static_assert(std::is_base_of(Control, ICSE::wnd::AWCanvasControl), "The Control must inherit AWCanvasControl.");
+	static_assert(std::is_base_of<ICSE::wnd::AWCanvasControl, Control>::value, "The Control must inherit AWCanvasControl.");
 
 	std::shared_ptr<Control> ctl{ new Control(args) };
 	m_controls.insert(ctl->id(), ctl);
