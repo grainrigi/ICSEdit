b6977eef807ba1c9c4f396cd4e69cf411aed09c0
NKiryu <naonaokiryu2@gmail.com>
2017-03-22 19:52:35 +0900

    Implemented WStaticText

--- ICSEdit/cpp/wnd/WStaticText.cpp
+++ ICSEdit/cpp/wnd/WStaticText.cpp
@@ -20,3 +20,135 @@ along with ICSEdit.  If not, see <http://www.gnu.org/licenses/>.
 #include "wnd/WStaticText.h"
 
 using namespace ICSE::wnd;
+
+ICSE::wnd::WStaticText::WStaticText(std::shared_ptr<font::Font> font)
+	: m_renderer(font),
+	AWSizableControl(),
+	m_str(""),
+	m_rendered(false),
+	m_align(0),
+	m_opaqueness(255)
+{
+	dUpdate += [&]() { this->update(); };
+	this->AWControl::Resize += [&](int w, int h) { this->m_rendered = false; };
+}
+
+void ICSE::wnd::WStaticText::SetFontFamily(std::shared_ptr<font::Font> font)
+{
+	m_renderer.SetFontFamily(font);
+	m_rendered = false;
+}
+
+void ICSE::wnd::WStaticText::SetFontSize(int size)
+{
+	m_renderer.SetFontHeight(size);
+	m_rendered = false;
+}
+
+void ICSE::wnd::WStaticText::SetOutlineSize(int size)
+{
+	m_renderer.SetOutlineWidth(size);
+	m_rendered = false;
+}
+
+void ICSE::wnd::WStaticText::SetFontColor(const graphics::ColorDW & color)
+{
+	m_renderer.SetFontColor(color.UintRepr());
+	m_rendered = false;
+}
+
+void ICSE::wnd::WStaticText::SetFontColor(uint32_t color)
+{
+	m_renderer.SetFontColor(color);
+	m_rendered = false;
+}
+
+void ICSE::wnd::WStaticText::SetOutlineColor(const graphics::ColorDW & color)
+{
+	m_renderer.SetOutlineColor(color.UintRepr());
+	m_rendered = false;
+}
+
+void ICSE::wnd::WStaticText::SetOutlineColor(uint32_t color)
+{
+	m_renderer.SetOutlineColor(color);
+	m_rendered = false;
+}
+
+void ICSE::wnd::WStaticText::SetOpaqueness(uint8_t opaqueness)
+{
+	m_opaqueness = opaqueness;
+	m_rendered = false;
+}
+
+void ICSE::wnd::WStaticText::SetAlign(int align)
+{
+	m_align = align;
+	m_rendered = false;
+}
+
+void ICSE::wnd::WStaticText::SetText(const std::string & str)
+{
+	m_str = str;
+	m_rendered = false;
+}
+
+void ICSE::wnd::WStaticText::update(void)
+{
+	if(!m_rendered)
+	{
+		auto canvas = m_canvas.Lock();
+
+		auto start = std::chrono::high_resolution_clock::now();
+
+		int x, y;
+		graphics::RECT rect;
+		rect = m_renderer.GetRenderRect(m_str.c_str());
+
+		switch(m_align & 0xff)
+		{
+			case eWStaticTextAlign::Left:
+				x = 0;
+				break;
+			case eWStaticTextAlign::Right:
+				x = m_canvas.width() - rect.width() - 1;
+				break;
+			case eWStaticTextAlign::CenterHorz:
+				x = (m_canvas.width() - rect.width()) >> 1;
+				break;
+			default:
+				x = 0;
+				break;
+		}
+		switch(m_align & 0xff00)
+		{
+			case eWStaticTextAlign::Top:
+				y = 0;
+				break;
+			case eWStaticTextAlign::Bottom:
+				y = m_canvas.height() - rect.height() - 1;
+				break;
+			case eWStaticTextAlign::CenterVert:
+				y = (m_canvas.height() - rect.height()) >> 1;
+				break;
+			default:
+				y = 0;
+				break;
+		}
+		x = x < 0 ? 0 : x;
+		y = y < 0 ? 0 : y;
+		
+		canvas->ClearWithColor(0x00000000);
+		m_renderer.RenderText(*canvas, m_str.c_str(), x, y, m_opaqueness);
+
+		auto end = std::chrono::high_resolution_clock::now();
+
+		m_cnt = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+		m_canvas.Unlock();
+
+		m_rendered = true;
+	}
+}
+
+
--- ICSEdit/include/wnd/WStaticText.h
+++ ICSEdit/include/wnd/WStaticText.h
@@ -18,16 +18,49 @@ along with ICSEdit.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
+#include "wnd/AWSizableControl.h"
+#include "util/font/FontRenderer.h"
 
 namespace ICSE {
 namespace wnd {
 
-	class WStaticText {		
-	 public:
-		WStaticText(void);
+	enum eWStaticTextAlign
+	{
+		Left = 0x0001,
+		Right = 0x0002,
+		CenterHorz = 0x0003,
+		Top = 0x0100,
+		CenterVert = 0x0200,
+		Bottom = 0x0300,
+	};
+
+	class WStaticText : public AWSizableControl {
+		font::FontRenderer m_renderer;
+		std::string m_str;
+		bool m_rendered;
+		int m_cnt;
+		int m_align;
+		uint8_t m_opaqueness;
+	public:
+		WStaticText(std::shared_ptr<font::Font> font);
+
+		void SetFontFamily(std::shared_ptr<font::Font> font);
+		void SetFontSize(int size);
+		void SetOutlineSize(int size);
+
+		void SetFontColor(const graphics::ColorDW &color);
+		void SetFontColor(uint32_t color);
+		void SetOutlineColor(const graphics::ColorDW &color);
+		void SetOutlineColor(uint32_t color);
+
+		void SetOpaqueness(uint8_t opaqueness);
+		void SetAlign(int align);
+
+		void SetText(const std::string &str);
+		std::string GetText(void) const { return this->m_str; }
 		
-	 private:
-		
+	private:
+		 void update(void);
 	};
 
 }
